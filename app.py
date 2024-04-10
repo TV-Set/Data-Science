bfe0f3fdaff53499743bc81334a8365442d4ccfa
TV-Set <nikolay-polyakov@mail.com>
2024-04-10 18:03:05 +0300

    Добавлен файл "index.html"

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 
 def print_hello():
-    return "<h1>Hello!</h1>"  # Заголовок первого уровня
+    return render_template("index.html")
 
 app.run()
