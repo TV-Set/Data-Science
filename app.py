a0de0f8e52d1682615da3280c5e8a519c8bd8971
TV-Set <nikolay-polyakov@mail.com>
2024-04-11 01:48:44 +0300

    Изменены файлы "app.py" и "index.html"

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["get", "post"]) # 127.0.0.1:5000 + "/" = 127.0.0.1:5000/
 
-def print_hello():
-    return render_template("index.html")
+def index(): # Данная функция вызывается с помощью декоратора "@app.route()"
+    message = "Ничего не введено"
+    if request.method == "POST":
+        density = request.form.get("density")
+        print(density)
+        message = f"Вы ввели: {density}"
+
+    return render_template("index.html", message=message)
 
 app.run()
